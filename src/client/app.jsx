ca4b1a960d54a34fdbe74a82ee5c54f803142813
Kevin Li <li.ctrl.z@gmail.com>
2019-02-26 13:39:09 -0500

    Implement naive retrieval/render of db.json

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,18 +1,55 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      omo: 'omo'
+      events: [],
     };
+
+    this.updateEvents = this.updateEvents.bind(this);
+    this.getEvents = this.getEvents.bind(this);
   };
+
+  updateEvents(data) {
+    this.setState({
+      events: data,
+    });
+  }
+
+  getEvents() {
+    axios.get('http://localhost:3000/events')
+      .then(events => {
+        // isolate first 10 results
+        let eventList = [];
+        for (let i = 0; i < 10; i++) {
+          eventList.push(events.data[i]);
+        }
+        this.updateEvents(eventList);
+        console.log('pachoo');
+      })
+      .catch(err => {
+        console.log('ERR on data GET', err);
+      })
+  }
+
+  componentDidMount() {
+    this.getEvents();
+  }
 
   render() {
     return (
       <div>
-        {this.state.omo}
+        <ul>
+          {this.state.events.map(event => {  
+            let output = event.description;
+            return (
+              <li>{output}</li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
