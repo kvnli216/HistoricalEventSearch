8c4e5da4549f2216d09c121234dc7433652ab007
Kevin Li <li.ctrl.z@gmail.com>
2019-03-04 23:31:21 -0500

    Implement basic styling

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import reactDOM from 'react-dom';
 import React from 'react';
-import App from './client/app.jsx';
+import App from './client/App.jsx';
 
 reactDOM.render(<App perPage={10}/>, document.getElementById('app'));
